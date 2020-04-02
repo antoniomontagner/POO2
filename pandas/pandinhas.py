5457183aecc738800c553163340f2bc9ea70af9d
jaca <antoniomontagner@hotmail.com>
2020-04-01 21:53:25 -0500

    add

--- pandas/pandinhas.py
+++ pandas/pandinhas.py
@@ -1,9 +1,19 @@
 import pandas as pd 
+import numpy as np
 
 dic = {
     "nome":["a","b"],
     "nota":[1,2],
     "aprovado":["sim","nao"]
 }
-df = pd.DataFrame(dic)
+df = pd.DataFrame(dic)  
 print(df)
+
+ff = pd.Series(dic)
+print (ff)
+
+a = [1,2,3,4,5]
+ar = np.array(a)
+rr = np.array([(1,2,3,4,5),(1,2,3,4,5)])
+print(ar)
+print(rr)
--- revisao/excecoes.py
+++ revisao/excecoes.py
@@ -1,4 +1,4 @@
-try:
+"""try:
     a = int(input("N: "))
     if not 1<=a<= 20:
         raise ValueError
@@ -9,3 +9,28 @@ except ValueError:
     print("Jaca")
 finally:
     print(a)
+"""
+
+lista = []
+for i in range(3):
+    while True:
+        try:
+            n = int(input("N: "))
+            if n in [1,2]:
+                break
+            else:
+                raise ValueError
+        except IndexError:
+            pass
+        except ValueError:
+            # assert n != [1,2], 'sefude' #comentario para quando da erro
+            pass
+        except (IndexError, ValueError):
+            print("JACA")
+            pass
+        except:     #abrange todas as excecoes< tem que colocar porultimo
+            pass
+        finally:
+            print("bitch")
+    lista.append(n)
+print(lista)
